a955d1a1e58a0d1dec38cf9ec2c3babadaecba47
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-10-02 21:38:23 -0400

    Spacing and heading size fixed

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Intro = () => (
   <div className="text-center">
-    <h1 className="text-2xl text-gray-900">
+    <h1 className="text-3xl text-gray-800">
       VIN Decoder
     </h1>
 
-    <p className="text-sm text-gray-700">
+    <p className="mt-2 text-sm text-gray-600">
       v1.0
     </p>
   </div>
