2ce811d58c0553dda80bcdeaf76dc5224ee8e704
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-10-02 21:20:23 -0400

    Intro added

---
+++ src/components/Intro.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Intro = () => (
+  <div className="text-center">
+    <h1 className="text-2xl text-gray-900">
+      VIN Decoder
+    </h1>
+
+    <p className="text-sm text-gray-700">
+      v1.0
+    </p>
+  </div>
+);
+
+export default Intro;
