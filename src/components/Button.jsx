ffe1ca341f745b1e5c63ac2345a02c952095604d
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-10-02 21:47:02 -0400

    Code fixed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Button = ({ children, className, ...rest }) => (
-  <button className={`m-2 px-4 py-2 rounded uppercase tracking-wider ${className}`} {...rest}>
+  <button
+    className={`m-2 px-4 py-2 rounded uppercase tracking-wider ${className}`}
+    {...rest}
+  >
     {children}
   </button>
 );
