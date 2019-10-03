4e7db82a8c62caf12bfa61b0e6c32e08dcb4a6e1
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-10-02 21:45:40 -0400

    Form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import Intro from './components/Intro';
+import Button from './components/Button';
+
 
 const App = () => (
   <div className="min-h-screen bg-gray-200 flex justify-center">
     <div className="w-full max-w-lg py-24">
       <Intro />
 
-      <form
-        className="mt-12"
-      >
+      <form className="mt-12">
         <input
           className="w-full p-3 rounded border-2 border-gray-300 focus:border-gray-400"
           placeholder="Enter VIN"
         />
 
         <div className="mt-2 flex justify-center">
-          <button className="m-2 bg-gray-300 hover:bg-gray-400 text-gray-800 px-4 py-2 rounded uppercase tracking-wider">
+          <Button className="bg-gray-300 hover:bg-gray-400 text-gray-800">
             View Code
-          </button>
+          </Button>
 
-          <button className="m-2 bg-blue-600 hover:bg-blue-700 text-blue-100 px-4 py-2 rounded uppercase tracking-wider">
+          <Button className="bg-blue-600 hover:bg-blue-700 text-blue-100">
             Submit
-          </button>
+          </Button>
         </div>
       </form>
     </div>
