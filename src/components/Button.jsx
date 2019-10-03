ccbcdb9c73cbf2ccb735bd2caa63d9f3228340f7
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-10-02 21:50:22 -0400

    Proptypes added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Button = ({ children, className, ...rest }) => (
+  // eslint-disable-next-line react/button-has-type
   <button
     className={`m-2 px-4 py-2 rounded uppercase tracking-wider ${className}`}
+    // eslint-disable-next-line react/jsx-props-no-spreading
     {...rest}
   >
     {children}
   </button>
 );
+
+Button.propTypes = {
+  className: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+};
 
 export default Button;
