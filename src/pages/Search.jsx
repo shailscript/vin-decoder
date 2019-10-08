2db204350d7402787b8d322063aba8dfce08ee52
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-10-08 12:28:22 -0400

    simplified

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,51 +1,40 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router';
-import { parse } from 'qs';
 import { apiKey } from '../config';
+import withQuery from '../hocs/withQuery';
 import Intro from '../components/Intro';
 import Loader from '../components/Loader';
 import SearchForm from '../components/SearchForm';
-
-const withQuery = (Component) => ({ location: { search } }) => {
-  const searchCleaned = search.replace('?', '');
-  const query = parse(searchCleaned);
-  return (
-    <Component {...query} />
-  );
-};
 
 const SearchPage = ({ vin }) => {
   const history = useHistory();
 
   const handleSearchAction = (validVin) => {
-    console.log('about to change history');
     history.push(`/search?vin=${validVin}`);
   };
 
-  const requestParams = {
-    url: `http://marketcheck-prod.apigee.net/v1/vin/${vin}/specs?api_key=${apiKey}`,
-    params: {
-      method: 'GET',
-    },
-  };
+  const url = `http://marketcheck-prod.apigee.net/v1/vin/${vin}/specs?api_key=${apiKey}`;
+
+  const rLoader = <Loader url={url} />;
+  const rNoLoader = null;
+
+  const rSearchForm = (
+    <SearchForm
+      vin={vin}
+      onSubmit={handleSearchAction}
+    />
+  );
 
   return (
     <div className="w-full max-w-lg py-24">
       <Intro />
 
       <div className="mt-12">
-        <SearchForm
-          vin={vin}
-          handleSearchAction={handleSearchAction}
-        />
+        {rSearchForm}
       </div>
 
-      {vin
-        ? <Loader requestParams={requestParams} />
-        : null}
-
+      {vin ? rLoader : rNoLoader}
     </div>
   );
 };
