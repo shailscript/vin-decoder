1fddc9635546fa86d3e00cc757273fe1f8c9c34e
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-10-08 12:10:23 -0400

    component imprived

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Error = ({ message }) => (
-  <div className="w-full my-2 p-3 rounded bg-red-300">
-    <p className="text-md text-red-900">
-      <i className="px-4 fas fa-exclamation-circle" />
+  <div className="w-full my-2 p-3 rounded bg-red-100 border-l-4 border-red-600">
+    <div className="text-md text-red-900">
+      <i className="pr-3 fas fa-exclamation-circle opacity-75" />
       {message}
-    </p>
+    </div>
   </div>
 );
 
